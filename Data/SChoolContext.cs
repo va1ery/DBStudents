71479d93646f3aae6255f3391f246ad370bc1e53
Valery Gurov <va1ery@ya.ru>
2020-03-22 23:01:40 +0300

    project files added

---
+++ Data/SChoolContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using SchoolDB.Models;
+
+namespace SchoolDB.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite(@"Data Source=DBDtudents.db");
+            }
+        }
+    }
+}
